ae0dd98132fd0540f5f59e76d506f28745f14c5e
bintang <astawabintang@gmail.com>
2026-06-02 23:30:29 +0800

    fix user schema and auth controller

--- src/controller/authRegisterController.ts
+++ src/controller/authRegisterController.ts
@@ -2,19 +2,23 @@ import type { Request, Response } from "express";
 import * as z from "zod";
 import validateRegisterSchema from "../type/validateRegisterSchema.ts";
 import errorMessageZod from "../type/errorMessageZod/errorMessageZod.ts";
-
+import userModel from "../model/user.model.ts";
 
 const register = async (req: Request, res: Response) => {
   try {
     const validReq = validateRegisterSchema.parse(req.body);
     //cek ke db
-    
-    // const userExist = await 
-    //test db
-    
+    const existingUser = await userModel.findOne({ email: validReq.email });
+    if (existingUser) {
+      return res.status(409).json({ message: "Email sudah terdaftar" });
+    }
+    const result = await userModel.create({
+      ...validReq,
+    });
+
     res.status(200).json({
       message: "berhasil register",
-      data: `${(validReq.username, validReq.email)}`,
+      data: `${result.email}`,
     });
   } catch (err) {
     if (err instanceof z.ZodError) {
--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -9,11 +9,15 @@ const userSchema = new schema<userInterface>(
     username: { type: schema.Types.String, required: true },
     age: { type: schema.Types.Number, required: true },
     email: { type: schema.Types.String, required: true },
-    password: { type: schema.Types.String, enum: ["admin", "user"], default: "user" },
-    role: { type: schema.Types.String, required: true },
+    password: { type: schema.Types.String,required: true },
+    role: { type: schema.Types.String,enum: ["admin", "user"], default: "user"},
     profilePicture: { type: schema.Types.String, default: "user.jpg" },
     isActive: { type: schema.Types.Boolean, default: false },
     activationCode: { type: schema.Types.String },
   },
   { timestamps: true },
 );
+
+const userModel = mongoose.model("user",userSchema)
+
+export default userModel;
